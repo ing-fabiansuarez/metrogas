29b18b0a37c675c20d5dbecaa00d358e4b09072e
Fabian Enrique Suarez Carvajal <fsuarez@MTG.local>
2022-06-10 10:44:19 -0500

    Algunos ajustes para el navbar

--- resources/views/layouts/navbars/auth/nav.blade.php
+++ resources/views/layouts/navbars/auth/nav.blade.php
@@ -11,7 +11,7 @@
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4 d-flex justify-content-end" id="navbar"> 
             <div class="nav-item d-flex align-self-end">
                 <a href="https://www.creative-tim.com/product/soft-ui-dashboard-laravel" target="_blank" class="btn btn-primary active mb-0 text-white" role="button" aria-pressed="true">
-                    Download
+                    Crear Solicitud
                 </a>
             </div>
             <div class="ms-md-3 pe-md-3 d-flex align-items-center">
